b0fa464fd491745a9c3a4ca98123cafd5009d563
maxvanbel <maxvanbel@gmail.com>
2017-05-08 11:46:12 -0500

    add preventDefault() to form

--- weather/src/containers/search_bar.js
+++ weather/src/containers/search_bar.js
@@ -7,6 +7,7 @@ export default class SearchBar extends Component {
     this.state = { term: '' };
 
     // We need to bind this function to SearchBar, we get an err otherwise
+    // you could also make the onInputCanhge function a fat arrow
     this.onInputChange = this.onInputChange.bind(this);
   }
 
@@ -15,19 +16,24 @@ export default class SearchBar extends Component {
     this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    // This is so that the form doesn't submit to the back end
+    // We use FORM so that we can press ENTER or press the SUBMIT btn
+    event.preventDefault();
+  }
 
   render() {
     return (
-    <form className="input-group">
-      <input
-        placeholder="Get a five-day forecast in your favorite cities"
-        className="form-control"
-        value={this.state.term}
-        onChange={this.onInputChange} />
-      <span className="input-group-btn">
-        <button type="submit" className="btn btn-secondary">Submit</button>
-      </span>
-    </form>
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
     );
   }
 }
